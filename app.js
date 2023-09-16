2e50da94a9854eb564a62e5079ee468d465792b5
Spich <spichakovda@gmail.com>
2023-09-16 14:25:14 -0400

    int id generation added

--- app.js
+++ app.js
@@ -16,10 +16,16 @@ fs.readFile(filePath + fileName, 'utf8', (err, data) => {
         const jsonArray = JSON.parse(data);
 
         // Add IDs to objects and move them to the first position
-        for (let i = 0; i < jsonArray.length; i++) {
-            const id = uuidv4();
-            jsonArray[i] = { id, ...jsonArray[i] }; // Add the ID at the first position
-        }
+        // for (let i = 0; i < jsonArray.length; i++) {
+        //     const id = uuidv4();
+
+        //     jsonArray[i] = { id, ...jsonArray[i] }; // Add the ID at the first position
+        // }
+
+        // Add IDs to objects as integer index
+        jsonArray.forEach((item, index) => {
+            item.id = index + 1; // Assign integer index as the ID
+        });
 
         // Write updated data back to file
         fs.writeFile(filePath + newFileName, JSON.stringify(jsonArray, null, 2), 'utf8', err => {
